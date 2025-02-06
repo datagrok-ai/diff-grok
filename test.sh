6f9d62e51bfdcd82aec06c2ae1b55f9c6f51f917
Vladyslav Hlushchenko <vhlushchenko@datagrok.ai>
2025-02-06 13:48:50 +0200

    ds-tools: gh pipeline

--- test.sh
+++
@@ -1,3 +0,0 @@
-if [[ "$(jq '.name' package.json | sed -E 's/(^"|"$)//g')" != "@datagrok/diff-studio-tools" ]]; then
-    echo "::error title=ds-tools: failed properties check::Library should be in '@datagrok/diff-studio-tools' scope. Change library name to '@datagrok-libraries/<name>' in /package.json"
-fi
